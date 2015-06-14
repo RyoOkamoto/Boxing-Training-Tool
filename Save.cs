0105064c7931c6708187d3e870a16d79c33555b0
ryo <ryo@shirai.la>
2015-06-15 01:49:42 +0900

    SaveとReadの改良

--- Save.cs
+++ Save.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Microsoft.Kinect;
 
 namespace BoxingSensor
 {
@@ -13,8 +14,8 @@ namespace BoxingSensor
     {
 
 
-        //CSVファイルにポーズ情報書き込む
-        public static void PosSave(double num, double num2, double num3)
+        //CSVファイルにデータ情報書き込む
+        public static void DataSave(double num, double num2, double num3)
         {
             StreamWriter sw;
             FileInfo fi;
@@ -24,3 +25,95 @@ namespace BoxingSensor
             sw.Flush();
             sw.Close();
         }
+
+
+        //CSVファイルにポーズ情報書き込む
+        public static void PosSave1(double num, double num2, double num3)
+        {
+            StreamWriter sw;
+            FileInfo fi;
+            fi = new FileInfo(@"C:\Users\labohp\Documents\Visual Studio 2013\Projects\BoxingSensor\BoxingSensor\/Pose1.csv");
+            sw = fi.AppendText();
+            sw.WriteLine(num + "," + num2 + "," + num3);
+            sw.Flush();
+            sw.Close();
+        }
+
+        //CSVファイルにポーズ情報書き込む
+        public static void PosSave2(double num, double num2, double num3)
+        {
+            StreamWriter sw;
+            FileInfo fi;
+            fi = new FileInfo(@"C:\Users\labohp\Documents\Visual Studio 2013\Projects\BoxingSensor\BoxingSensor\/Pose2.csv");
+            sw = fi.AppendText();
+            sw.WriteLine(num + "," + num2 + "," + num3);
+            sw.Flush();
+            sw.Close();
+        }
+
+        //CSVファイルからポーズの3点の座標データを読取って,その値を返す.
+        public static void ReadFile(ref double []x,double []y,double []z)
+        {
+           
+                //ファイルパスの宣言
+            FileInfo fi = new FileInfo(@"C:\Users\labohp\Documents\Visual Studio 2013\Projects\BoxingSensor\BoxingSensor\/Pose1.csv");
+                //ファイル読み込み準備
+                StreamReader sr = new StreamReader(fi.OpenRead());
+
+                //一行ずつデータを読取って格納
+                for (int i = 0; i < 6; i++)
+                {
+                    String str = sr.ReadLine();
+                    String[] str2 = str.Split(',');
+
+                    x[i] = float.Parse(str2[0]);
+                    y[i] = float.Parse(str2[1]);
+                    z[i] = float.Parse(str2[2]);
+
+                    
+                }
+                //ファイル読み込み後処理（クローズ）
+                sr.Close();
+            
+        }
+
+        //CSVファイルからポーズの3点の座標データを読取って,その値を返す.
+        public static void ReadFile2(ref double[] x, double[] y, double[] z)
+        {
+
+            //ファイルパスの宣言
+            FileInfo fi = new FileInfo(@"C:\Users\labohp\Documents\Visual Studio 2013\Projects\BoxingSensor\BoxingSensor\/Pose2.csv");
+            //ファイル読み込み準備
+            StreamReader sr = new StreamReader(fi.OpenRead());
+
+            //一行ずつデータを読取って格納
+            for (int i = 0; i < 6; i++)
+            {
+                String str = sr.ReadLine();
+                String[] str2 = str.Split(',');
+
+                x[i] = float.Parse(str2[0]);
+                y[i] = float.Parse(str2[1]);
+                z[i] = float.Parse(str2[2]);
+
+
+            }
+            //ファイル読み込み後処理（クローズ）
+            sr.Close();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+       
+    }
+}
+
