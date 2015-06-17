617610b689446313c4519106455510548787832e
ryo <ryo@shirai.la>
2015-06-17 10:21:24 +0900

    Save改良

--- Save.cs
+++ Save.cs
@@ -27,7 +27,7 @@ namespace BoxingSensor
         }
 
 
-        //CSVファイルにポーズ情報書き込む
+        //CSVファイルにポーズ情報書き込む（構え）
         public static void PosSave1(double num, double num2, double num3)
         {
             StreamWriter sw;
@@ -39,7 +39,7 @@ namespace BoxingSensor
             sw.Close();
         }
 
-        //CSVファイルにポーズ情報書き込む
+        //CSVファイルにポーズ情報書き込む（右ストレート）
         public static void PosSave2(double num, double num2, double num3)
         {
             StreamWriter sw;
@@ -50,6 +50,21 @@ namespace BoxingSensor
             sw.Flush();
             sw.Close();
         }
+
+        //CSVファイルにポーズ情報書き込む（左ジャブ）
+        public static void PosSave3(double num, double num2, double num3)
+        {
+            StreamWriter sw;
+            FileInfo fi;
+            fi = new FileInfo(@"C:\Users\labohp\Documents\Visual Studio 2013\Projects\BoxingSensor\BoxingSensor\/Pose3.csv");
+            sw = fi.AppendText();
+            sw.WriteLine(num + "," + num2 + "," + num3);
+            sw.Flush();
+            sw.Close();
+        }
+
+
+
 
         //CSVファイルからポーズの3点の座標データを読取って,その値を返す.
         public static void ReadFile(ref double []x,double []y,double []z)
@@ -103,7 +118,31 @@ namespace BoxingSensor
 
         }
 
+        //CSVファイルからポーズの3点の座標データを読取って,その値を返す.
+        public static void ReadFile3(ref double[] x, double[] y, double[] z)
+        {
 
+            //ファイルパスの宣言
+            FileInfo fi = new FileInfo(@"C:\Users\labohp\Documents\Visual Studio 2013\Projects\BoxingSensor\BoxingSensor\/Pose3.csv");
+            //ファイル読み込み準備
+            StreamReader sr = new StreamReader(fi.OpenRead());
+
+            //一行ずつデータを読取って格納
+            for (int i = 0; i < 6; i++)
+            {
+                String str = sr.ReadLine();
+                String[] str2 = str.Split(',');
+
+                x[i] = float.Parse(str2[0]);
+                y[i] = float.Parse(str2[1]);
+                z[i] = float.Parse(str2[2]);
+
+
+            }
+            //ファイル読み込み後処理（クローズ）
+            sr.Close();
+
+        }
 
 
 
